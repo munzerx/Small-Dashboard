1642f8796b387e22c08cb46ac5dca818f5201ab0
Munzer Mardini <munzerx@gmail.com>
2025-09-03 11:41:47 +0300

    add ability to add additional styles

--- Components/UI/Button.tsx
+++ Components/UI/Button.tsx
@@ -4,15 +4,17 @@ export default function Button({
   bgColor,
   text,
   textColor,
+  additionalStyles,
   ...props
 }: {
   bgColor: string;
   text: string;
   textColor?: string;
+  additionalStyles?: string;
 }) {
   return (
     <button
-      className="px-4 py-2 rounded-md hover:cursor-pointer hover:opacity-85"
+      className={`px-4 py-2 rounded-md hover:cursor-pointer hover:opacity-85 ${additionalStyles}`}
       style={{ backgroundColor: bgColor, color: textColor }}
       {...props}
       type="button"
