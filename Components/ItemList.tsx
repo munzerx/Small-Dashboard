3a03ab6a739c2edeed60e4ee4e67686e52c397ca
Munzer Mardini <munzerx@gmail.com>
2025-09-03 13:14:19 +0300

    fix map error from item list

---
+++ Components/ItemList.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Item from "./Item";
+import { mockItems } from "../public/data";
+
+interface ItemListProps {
+  onItemClick: (id: string) => void;
+}
+
+export default function ItemList({ onItemClick }: ItemListProps) {
+  return (
+    <div className="flex flex-col lg:w-[60%] px-5 md:px-0 gap-10">
+      <div>Search & Filtering</div>
+      <div className="flex flex-col">
+        <p className="font-bold text-md">Items</p>
+        <p className="text-sm text-slate-500">Manage Items</p>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        {mockItems.map((item: any) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            status={item.status}
+            onClick={() => onItemClick(item.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
