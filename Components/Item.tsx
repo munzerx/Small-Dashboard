b90fc15858b1c9613f602ba95e0ab865f88b5710
Munzer Mardini <munzerx@gmail.com>
2025-09-03 12:42:44 +0300

    add props to Item component

--- Components/Item.tsx
+++ Components/Item.tsx
@@ -1,34 +1,24 @@
 import React from "react";
 import Button from "./UI/Button";
-import { ChevronLeft, ChevronRight, Eye, Calendar, Clock } from "lucide-react";
 import { StatusBadge } from "./StatusBadge";
+import { ItemProps } from "@/Types/item";
 
-export default function Item() {
+export default function Item({ id, title, description, status }: ItemProps) {
   return (
     <div
-      //   key={item.id}
-      className="w-full border-slate-300 rounded-2xl border-[0.2px] p-4 hover:shadow-md transition-shadow cursor-pointer"
+      key={id}
+      className="w-full bg-white border-slate-300 rounded-2xl border-[0.2px] p-4 hover:shadow-md transition-shadow cursor-pointer"
       //   onClick={() => onItemClick(item.id)}
     >
       <div className="flex items-start justify-between">
         <div className="flex-1 min-w-0">
           <div className="flex items-center gap-3 mb-2">
-            <span className="text-xs font-mono text-slate-400">
-              {/* #{item.id} */}
-              #1
-            </span>
-            {/* <StatusBadge status={item.status} /> */}
-            <StatusBadge status={"active"} />
+            <span className="text-xs font-mono text-slate-400">#{id}</span>
+            <StatusBadge status={status} />
           </div>
 
-          <h3 className="font-medium mb-2 text-balance">
-            {/* {item.title} */}
-            Hello
-          </h3>
-          <p className="text-sm line-clamp-2 text-pretty">
-            {/* {item.description} */}
-            veryyyyyyyyyyyyyy longggggggggggg hellooooooooooooooooooo
-          </p>
+          <h3 className="font-medium mb-2 text-balance">{title}</h3>
+          <p className="text-sm line-clamp-2 text-pretty">{description}</p>
         </div>
 
         <Button
